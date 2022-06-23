645ce2bb14034efd5f9eaf79ef9ecd80d7ddb771
Silvia <silvia.mihit@gmail.com>
2022-06-23 13:20:02 +0200

    Time: 106 ms (18.28%), Space: 41.6 MB (93.43%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if (s.length === 0)
+        return true;
+    
+    if (s.length > t.length)
+        return false;
+    
+    let i = 0, j = 0;
+    while (i < s.length && j < t.length) {
+        if (s.charAt(i) === t.charAt(j)) {
+            i ++;
+            j ++;
+        } else 
+            j ++;
+        
+        if (i === s.length) 
+            return true;
+    }
+    
+    return false;
+};

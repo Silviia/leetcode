ab4b4c1552cfa71f6459493225bf1ad27421ee59
Silvia <silvia.mihit@gmail.com>
2022-07-10 20:19:07 +0200

    Time: 78 ms (77.98%), Space: 42.1 MB (34.82%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,45 @@
+const cache = {0: 0, 1: 1};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) { // Top-down approach using memoization, time, space complexity O(n)
+    if (n <= 1)
+        return n;
+    
+    if (cache[n] !== undefined)
+        return cache[n];
+    
+    cache[n] = fib(n-1) + fib(n-2);
+    
+    return cache[n];
+};
+
+// Iterative aproach, times, space complexity: O(N)
+// Bottom-up
+var fibIterative = function(n) {
+    if (n <= 1)
+        return n;
+    
+    
+    const f = [];
+    f [0] = 0, f[1] = 1;
+    
+    for (let i = 2; i <= n; i ++)
+        f[i] = f[i - 1] + f[i - 2];
+    
+    return f[n];
+};
+
+// Recursive aproach, high complexity
+var fibRec = function(n) {
+    if (n === 0)
+        return 0;
+    
+    if (n === 1)
+        return 1;
+    
+    if (n > 1)
+        return fib(n-1) + fib(n-2);
+};

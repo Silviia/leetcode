4482f17c194557c2077cf020a2b61a74fe65cd2d
Silvia <silvia.mihit@gmail.com>
2022-07-11 19:18:05 +0200

    Time: 115 ms, Memory: 51.6 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const map = {};
+    let containsDuplicates = false;
+    
+    for (let i = 0; i < nums.length; i ++) {
+        let val = nums[i]; 
+        if (!map[val]) 
+            map[val] = 1;
+        else {
+            containsDuplicates = true;
+            break;
+        }
+    }
+    
+    return containsDuplicates;
+};

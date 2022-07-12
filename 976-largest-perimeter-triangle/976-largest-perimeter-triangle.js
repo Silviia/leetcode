211ef3f41027f40c860bb4fe1bedcaba37c17e64
Silvia <silvia.mihit@gmail.com>
2022-07-12 11:55:28 +0200

    Time: 168 ms (24.96%), Space: 45.2 MB (76.96%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -1,9 +1,13 @@
 /**
+ * Find largest perimeter by first sorting the array.
+ * Valid triangle if the sum of two sides is bigger than the third.
+ * Since array is sorted we can take advantage of that to return the max perimeter.
  * @param {number[]} nums
  * @return {number}
  */
 var largestPerimeter = function(nums) {
     nums.sort((a, b) => a - b);
+   
     for (let i = nums.length - 1; i >= 0 ; i--) {
         if (nums[i - 1] + nums[i] > nums[i + 1]) 
              return nums[i - 1] + nums[i] + nums[i + 1];

07fb17dfd7485dce2d2b41a776cd13ae9dae647c
Silvia <silvia.mihit@gmail.com>
2022-07-13 12:46:45 +0200

    Time: 118 ms (11.05%), Space: 42.6 MB (75.33%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -4,13 +4,17 @@
  */
 var arraySign = function(nums) {
     let p = 1;
+    let countNeg = 0;
     
     for (let i = 0; i < nums.length; i ++) {
         if (nums[i] === 0)
-            return 0;
+            return signFunc(0);
         
-        p*= nums[i];
+        if (nums[i] < 0) 
+            countNeg++;
     }
+ 
+    p = (countNeg % 2 === 1) ? -1 : 1;
     
     return signFunc(p);
 };

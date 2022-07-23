22135b4e10fe6f3d4f26894f28cf20914463e37f
Silvia <silvia.mihit@gmail.com>
2022-07-24 00:00:51 +0200

    Time: 88 ms (87.94%), Space: 49.3 MB (44.48%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.js
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.js
@@ -7,11 +7,22 @@
  * }
  */
 /**
+ * Better space solution than the recursive method since is a constant space solution. O(1)
+ * Time complexity: O(H), where H is the height of the tree
  * @param {TreeNode} root
  * @param {number} val
  * @return {TreeNode}
  */
 var searchBST = function(root, val) {
+    while(root !== null && root.val !== val) {
+        root = val < root.val ? root.left : root.right;
+    }
+    
+    return root;
+};
+
+
+var searchBSTRecursive = function(root, val) {
     if (root === null || root.val === val) {
         return root;
     }

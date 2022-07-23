d38f79acca42065a1a8d17974c488b720ff5197b
Silvia <silvia.mihit@gmail.com>
2022-07-23 23:24:45 +0200

    Time: 86 ms (50.48%), Space: 42.5 MB (12.93%) - LeetHub

---
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+     if (!root) {
+        return [];
+    }
+    
+    let stack = [root], output = [];
+
+    while(stack.length > 0) {
+        let node = stack.pop();
+        
+        output.unshift(node.val);
+        
+        if (node.left) {
+            stack.push(node.left);
+        }
+        
+        if (node.right) {
+            stack.push(node.right);
+        }
+    }
+    
+    return output;
+};

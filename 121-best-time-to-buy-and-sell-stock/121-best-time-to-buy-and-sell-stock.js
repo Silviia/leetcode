d20b6f47d6700cad7e526e4fcafb0ce731b3b17b
Silvia <silvia.mihit@gmail.com>
2022-07-29 23:23:56 +0200

    Time: 88 ms (93.92%), Space: 52.1 MB (16.05%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,14 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let maxProfit = 0;
     let minPrice = Infinity;
+    let maxProfit = 0;
     
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < minPrice)
-            minPrice = prices[i];
-        else if (prices[i] - minPrice > maxProfit)
+    for (let i = 0; i < prices.length; i += 1) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else if (prices[i] - minPrice > maxProfit) {
             maxProfit = prices[i] - minPrice;
+        }
     }
     
     return maxProfit;
